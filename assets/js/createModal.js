688c1183eb0cd6cf4a35c90f4834df3c10ade3e2
harold-hurst <harry.hurst2636@gmail.com>
2025-06-05 14:01:28 +0100

    improve modal styling

---
+++ assets/js/createModal.js
@@ -0,0 +1,227 @@
+function createCard(string, country) {
+  console.log(country);
+  return `
+      <div
+        id="infoModal"
+        class="modal"
+        data-bs-backdrop="false"
+        tabindex="-1"
+      >
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content shadow">
+            <div class="modal-header bg-primary text-white">
+              <h5 class="modal-title">${string} ${country.country}</h5>
+              <button
+                type="button"
+                class="btn-close btn-close-white"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <table class="table table-striped">
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-street-view fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-heart fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-car fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-book fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-bath fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-bell fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-anchor fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-money-bill fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-wifi fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-globe fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-bicycle fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-landmark fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-tree fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-bookmark fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-bell fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-fire fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-key fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-newspaper fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-rocket fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-wheelchair fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+                <tr>
+                  <td class="text-center">
+                    <i class="fa-solid fa-monument fa-xl text-success"></i>
+                  </td>
+
+                  <td>Attribute</td>
+
+                  <td class="text-end">value</td>
+                </tr>
+              </table>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-outline-primary btn-sm"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+}
